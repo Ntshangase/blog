b83ffe8a3ca88d9dc8452e4ab1811b1d49d35f6f
Ntshangase <simphiwe323i@gmail.com>
2022-11-17 03:17:59 +0200

    working two pages

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 export default function Homepage() {
-  return (
-    <h1>Landing Page</h1>
-  )
+    return (
+        <div>
+            <h1>Landing Page</h1>
+           <text>This is where the first page will be</text>
+           <div><text>Click button below to go to Readmore page</text></div>
+           <a href="/Readmore">Readmore</a>
+        </div>
+    )
 }
--- src/components/Readmore.js
+++ src/components/Readmore.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Readmore() {
   return (
+    <div>
     <h2>Readmore Second page</h2>
+    <text>This is where the second page will be</text>
+    <div>
+    <a href="/">Homepage</a>
+    </div>
+    </div>
   )
 }
