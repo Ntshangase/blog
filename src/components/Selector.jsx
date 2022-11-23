1f746eb8fd39006ae56ee8dfd2cbcd847fa1fe6e
Ntshangase <simphiwe323i@gmail.com>
2022-11-23 14:09:20 +0200

    blue button

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Selector() {
   return (
     <div>
-      <button className="inline-flex px-2 overflow-visible text-white bg-gray-900 rounded group">
+      <button className=" inline-flex px-2  text-white bg-sky-500 hover:bg-sky-700 rounded group">
        
         <span className="pl-4 pr-5 py-2.5">Upload Button</span>
         <input
